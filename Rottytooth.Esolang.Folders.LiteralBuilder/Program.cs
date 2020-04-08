6e48f12cbd5b70368fb8a719e4b55ba06d59959d
Daniel Temkin <d@danieltemkin.com>
2020-04-07 21:38:46 -0400

    New prog eases encoding data into Folders

---
+++ Rottytooth.Esolang.Folders.LiteralBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rottytooth.Esolang.Folders.LiteralBuilder
+{
+    /// <summary>
+    /// A tool to read and write values
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Usage:");
+                Console.Error.WriteLine("LiteralBuilder type value [add_gitignore]");
+                Console.Error.WriteLine("type = char, int, float, string");
+                Console.Error.WriteLine("value = the value to convert");
+                Console.Error.WriteLine("[add_gitignore] (optional) allows for adding .gitignores to all terminal folders");
+                Console.Error.WriteLine("eg LiteralBuilder char e");
+                Console.Error.WriteLine("or LiteralBuilder string \"A test string\" true");
+            }
+
+            FolderTranslator.Write(args[0], args[1], true);
+        }
+    }
+}
